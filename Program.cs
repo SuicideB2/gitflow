5cc401a7ea19ff35d4b6b0970122868b024e7ccc
Ke4less <ke4less@vk.com>
2023-02-21 15:52:22 +0300

    number

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bulat");
+            Console.WriteLine("1523");
         }
     }
 }
